4691e222119db92ea140527f4876c93482ce35b0
petrero <petrero@gmail.com>
2020-01-25 12:29:14 +0200

    15.2.Webhook Endpoint Setup
    Decoding the Event
    Thanks to RequestBin, we know more or less what the JSON body will look like. The most important thing is this event id. Let's decode the JSON and grab this.
    To do that, add $data = json_decode(), but pause there. We need to pass this the body of the Request. In Symfony, we get this by adding a Request argument - don't forget the use statement! Then, use $request->getContent(). Also, pass true as the second argument so that json_decode returns an associative array.
    Next, it shouldn't happen, but just in case, if $data is null, that means Stripe sent us invalid JSON. Shame on you Stripe! Throw an exception in this case... and make sure you spell Exception correctly!
    Finally, get the  from $data['id']

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,7 +11,15 @@ class WebhookController extends AbstractController{
   /**
    * @Route("/webhooks/stripe", name="webhook_stripe")
    */
-  public function stripeWebhookAction(): Response{
+  public function stripeWebhookAction(Request $request): Response{
+  	$data = json_decode($request->getContent(), true);
+
+  	if($data === null){
+  		throw new \Exception('Bad JSON body from Stripe!');
+	  }
+
+  	$eventId = $data['id'];
+  	
     return new Response('baaa');
   }
 }
