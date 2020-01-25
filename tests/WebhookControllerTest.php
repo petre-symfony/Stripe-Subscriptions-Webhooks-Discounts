63f42c583aa32ab5aec31086f6acdadeb4dcf1ee
petrero <petrero@gmail.com>
2020-01-25 22:33:12 +0200

    17.5.Testing Webhooks{Prepping some Fake JSON}
    Prepping some Fake JSON
    To send the webhook, we first need to prepare a JSON string that matches what Stripe sends. At the bottom of the class, create a new private function called getCustomerSubscriptionDeletedEvent() with a  argument.
    To fill this in, go copy the real JSON from the test webhook. Paste it here with $json = <<<EOF enter, and paste! Now here's the important part: our controller reads the data.object.id key to find the subscription id. Replace this with %s. Then, finish the function with return sprintf($json, $subscriptionId).
    Now, this function will create a realistic-looking JSON string, but with whatever subscriptionId we want!
    Back in the test function, add $eventJson = $this->getCustomerSubscriptionDeletedEvent() and pass it $subscription->getStripeSubscriptionId(), which is some fake, random value that the function below created

--- tests/WebhookControllerTest.php
+++ tests/WebhookControllerTest.php
@@ -22,7 +22,9 @@ class WebhookControllerTest extends WebTestCase {
 	public function testStripeCustomerSubscriptionDeleted(){
 		$subscription = $this->createSubscription();
 
-		//to do send the cancelation webhook
+		$eventJson = $this->getCustomerSubscriptionDeletedEvent(
+			$subscription->getStripeSubscriptionId()
+		);
 
 		$this->assertFalse($subscription->isActive());
 	}
@@ -49,4 +51,127 @@ class WebhookControllerTest extends WebTestCase {
 
 	  return $subscription;
   }
+
+	private function getCustomerSubscriptionDeletedEvent($subscriptionId){
+		$json = <<<EOF
+		{
+      "created": 1326853478,
+      "livemode": false,
+      "id": "evt_00000000000000",
+      "type": "customer.subscription.deleted",
+      "object": "event",
+      "request": null,
+      "pending_webhooks": 1,
+      "api_version": "2017-02-14",
+      "data": {
+        "object": {
+            "id": "%s",
+            "object": "subscription",
+            "application_fee_percent": null,
+            "billing": "charge_automatically",
+            "billing_cycle_anchor": 1579883474,
+            "billing_thresholds": null,
+            "cancel_at": null,
+            "cancel_at_period_end": false,
+            "canceled_at": null,
+            "collection_method": "charge_automatically",
+            "created": 1579883474,
+            "current_period_end": 1582561874,
+            "current_period_start": 1579883474,
+            "customer": "cus_00000000000000",
+            "days_until_due": null,
+            "default_payment_method": null,
+            "default_source": null,
+            "default_tax_rates": [],
+            "discount": null,
+            "ended_at": 1579944968,
+            "invoice_customer_balance_settings": {
+                "consume_applied_balance_on_void": true
+            },
+            "items": {
+                "object": "list",
+                "data": [
+                    {
+                        "id": "si_00000000000000",
+                        "object": "subscription_item",
+                        "billing_thresholds": null,
+                        "created": 1579883474,
+                        "metadata": {},
+                        "plan": {
+                            "id": "plan_00000000000000",
+                            "object": "plan",
+                            "active": true,
+                            "aggregate_usage": null,
+                            "amount": 9900,
+                            "amount_decimal": "9900",
+                            "billing_scheme": "per_unit",
+                            "created": 1579847533,
+                            "currency": "usd",
+                            "interval": "month",
+                            "interval_count": 1,
+                            "livemode": false,
+                            "metadata": {},
+                            "name": "farmer_brent",
+                            "nickname": "farmer_brent_monthly",
+                            "product": "prod_00000000000000",
+                            "statement_descriptor": null,
+                            "tiers": null,
+                            "tiers_mode": null,
+                            "transform_usage": null,
+                            "trial_period_days": null,
+                            "usage_type": "licensed"
+                        },
+                        "quantity": 1,
+                        "subscription": "sub_00000000000000",
+                        "tax_rates": []
+                    }
+                ],
+                "has_more": false,
+                "url": "/v1/subscription_items?subscription=sub_GbiU6XOGM32zzy"
+            },
+            "latest_invoice": "in_1G4V3u26uSzZng1kYru1gy6d",
+            "livemode": false,
+            "metadata": {},
+            "next_pending_invoice_item_invoice": null,
+            "pending_invoice_item_interval": null,
+            "pending_setup_intent": null,
+            "pending_update": null,
+            "plan": {
+                "id": "plan_00000000000000",
+                "object": "plan",
+                "active": true,
+                "aggregate_usage": null,
+                "amount": 9900,
+                "amount_decimal": "9900",
+                "billing_scheme": "per_unit",
+                "created": 1579847533,
+                "currency": "usd",
+                "interval": "month",
+                "interval_count": 1,
+                "livemode": false,
+                "metadata": {},
+                "name": "farmer_brent",
+                "nickname": "farmer_brent_monthly",
+                "product": "prod_00000000000000",
+                "statement_descriptor": null,
+                "tiers": null,
+                "tiers_mode": null,
+                "transform_usage": null,
+                "trial_period_days": null,
+                "usage_type": "licensed"
+            },
+            "quantity": 1,
+            "schedule": null,
+            "start": 1579938984,
+            "start_date": 1579883474,
+            "status": "canceled",
+            "tax_percent": null,
+            "trial_end": null,
+            "trial_start": null
+        }
+      }
+    }
+EOF;
+		return sprintf($json, $subscriptionId);
+	}
 }
